791fdee14a08cc71530e12c7039ae907698e78b9
Lakshya soni <lakshyasoni0925@gmail.com>
2026-07-17 10:02:41 +0530

    Configure production domains inside ALLOWED_HOSTS array

--- Firstapp/Firstapp/settings.py
+++ Firstapp/Firstapp/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '://onrender.com']
 GOOGLE_CLIENT_ID = env('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = env('GOOGLE_CLIENT_SECRET')
 
